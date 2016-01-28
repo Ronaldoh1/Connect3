3f7e7dec9068646730ad421c0c5756f83a1da03c
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-01-28 00:10:12 -0500

    readjust layout

--- app/src/main/java/com/example/ronaldhernandez/connect3/MainActivity.java
+++ app/src/main/java/com/example/ronaldhernandez/connect3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ronaldhernandez.connect3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dropIn(View view){
+
+        ImageView counter = (ImageView)view;
+
+        //need to move it off the screen.Apply red or yellow.
+
+        counter.setTranslationY(-1000f);
+        //set the image.
+        counter.setImageResource(R.drawable.yellow);
+        //Move it back to position
+        counter.animate().translationYBy(1000f).rotation(180).setDuration(300);
+
+
     }
 }
